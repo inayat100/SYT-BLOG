7cd46dfae8bc727f5050b19433bd4fdd12973254
inayat100 <68042481+inayat100@users.noreply.github.com>
2022-02-04 23:25:54 +0530

    Update settings.py
    ok

--- myblog/settings.py
+++ myblog/settings.py
@@ -79,12 +79,34 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'd9f7m6rdkgvqt2',
+
+        'USER': 'ondedyukouaueb',
+
+        'PASSWORD': 'db341a1a8128b812eaceaea7b39b118d71753d30e10269276da128eedd5c50f2',
+
+        'HOST': 'ec2-23-20-73-25.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
     }
+
 }
+
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
