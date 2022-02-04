50e6140739cd12757f1b76f74880e0c6b6753dfb
inayat100 <68042481+inayat100@users.noreply.github.com>
2022-02-04 21:40:04 +0530

    Update urls.py
    ok

--- myblog/urls.py
+++ myblog/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('jet/', include('jet.urls','jet')),
+#     path('jet/', include('jet.urls','jet')),
     path('',include('app.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
